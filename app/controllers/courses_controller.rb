2c40c68ee88f3654cb773c6b1cfffcd8cb2aba00
Luiz Carlos Lima <llimadosanjos@gmail.com>
2021-05-19 00:13:32 -0300

    Cadastrar um curso

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,4 +7,25 @@ class CoursesController < ApplicationController
     @course = Course.find(params[:id])
   end
 
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to @course
+    else
+      flash[:alert] = 'Você deve informar todos os dados do curso'
+        render :new
+    end
+  end
+
+
+private
+
+def course_params
+  params.require(:course).permit(:name,:description,:code,:price,:enrollment_deadline)
+end
+
 end
